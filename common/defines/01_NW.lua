8ca897acd8fa22a7a25cd0c20104658d5273948e
DRAFTik <41925073+DRAFTik@users.noreply.github.com>
2022-08-07 23:22:01 +0300

    fix

--- common/defines/01_NW.lua
+++ common/defines/01_NW.lua
@@ -23,6 +23,8 @@ NDefines.NDiplomacy.VERY_BAD_OPINION = -100									-- Threshold for a country t
 NDefines.NDiplomacy.IDEOLOGY_JOIN_FACTION_MIN_LEVEL = 0.4					-- ideology limit required to join faction
 NDefines.NDiplomacy.DIPLOMACY_HOURS_BETWEEN_REQUESTS = 0					-- How long a country must wait before sending a new diplomatic request.
 NDefines.NDiplomacy.MIN_TRUST_VALUE = -500									-- max value for embargo, vanilla -100
+NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 50								-- Political power cost to send attache
+NDefines.NDiplomacy.BASE_SEND_ATTACHE_CP_COST = 25							-- Command Power sent attache usage cost
 
 -- Technology
 NDefines.NTechnology.MAX_SUBTECHS = 4										-- Max number of sub technologies a technology can have.
